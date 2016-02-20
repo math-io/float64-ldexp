0d931baa48d8044a487002f7627956fe6e59cae3
kgryte <kgryte@gmail.com>
2016-02-20 04:49:05 -0800

    Add edge cases and associated tests

--- test/test.js
+++ test/test.js
@@ -3,6 +3,8 @@
 // MODULES //
 
 var tape = require( 'tape' );
+var PINF = require( 'const-pinf-float64' );
+var NINF = require( 'const-ninf-float64' );
 var ldexp = require( './../lib' );
 
 
@@ -86,5 +88,37 @@ tape( 'the function multiplies a number by an integer power of two (subnormal `x
 		v = ldexp( frac[i], exp[i] );
 		t.equal( v, expected[i], 'frac: '+frac[i]+'; exp: '+exp[i]+'; expected: '+expected[i] );
 	}
+	t.end();
+});
+
+tape( 'if provided a fraction equal to `+0`, the function returns `+0`', function test( t ) {
+	var v = ldexp( 0, 10 );
+	t.equal( v, 0, 'returns 0' );
+	t.equal( 1/v, PINF, 'returns +0' );
+	t.end();
+});
+
+tape( 'if provided a fraction equal to `-0`, the function returns `-0`', function test( t ) {
+	var v = ldexp( -0, 10 );
+	t.equal( v, 0, 'returns 0' );
+	t.equal( 1/v, NINF, 'returns -0' );
+	t.end();
+});
+
+tape( 'if provided a fraction equal to `+infinity`, the function returns `+infinity`', function test( t ) {
+	var v = ldexp( PINF, 10 );
+	t.equal( v, PINF, 'returns +infinity' );
+	t.end();
+});
+
+tape( 'if provided a fraction equal to `-infinity`, the function returns `-infinity`', function test( t ) {
+	var v = ldexp( NINF, 10 );
+	t.equal( v, NINF, 'returns -infinity' );
+	t.end();
+});
+
+tape( 'if provided a fraction equal to `NaN`, the function returns `NaN`', function test( t ) {
+	var v = ldexp( NaN, 10 );
+	t.ok( v !== v, 'returns NaN' );
 	t.end();
 });
